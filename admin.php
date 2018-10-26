71f164c5ffe3a939be72123a99963034e93958c9
Aleksey <kladkan@yandex.ru>
2018-10-26 22:32:28 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,26 +1,28 @@
+<?php
+if (!empty($_FILES['test']['name'])) {
+    move_uploaded_file($_FILES['test']['tmp_name'], './tests/onserver'.$_FILES['test']['name']);
+    echo "Тест успешно загружен.<br>";
+    header('Location: ./list.php');
+    echo '<a href="list.php">Перейти к списку тестов прямо сейчас</a>';
+} else {
+    echo '<br>Ошибка загрузки теста! Вы не выбрали файл.';
+}
+
+?>
+
 <html>
 <head>
     <title>Загрузка тестов</title>
 </head>
 <body>
-<p><a href="list.php">Посмотреть список доступных тестов</a></p>
+
 <form action="admin.php" method="POST" enctype = "multipart/form-data">
     
     <p>Загрузите файл с тестом:</p>
     <p><input name="test" type="file"></p>
     <p><input type="submit" value="Загрузить"></p>
 </form>
+<p><a href="list.php">Посмотреть список доступных тестов</a></p>
 </body>
 </html>
 
-<?php
-
-if (!empty($_FILES['test']['name'])) {
-    move_uploaded_file($_FILES['test']['tmp_name'], './tests/onserver'.$_FILES['test']['name']);
-    echo "Тест успешно загружен.<br>";
-    echo '<a href="list.php">Перейти к списку тестов</a>';
-} else {
-    echo 'Ошибка загрузки! Вы не выбрали файл.';
-}
-
-?>
