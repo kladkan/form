59757f65c80fa308cd76e54477719403aa94400d
Aleksey <kladkan@yandex.ru>
2018-10-29 17:49:45 +0300

    Update list.php

--- list.php
+++ list.php
@@ -1,18 +1,24 @@
 <?php
-echo 'Список доступных тестов:<br><br>';
 
-if (@file_get_contents(__DIR__ . '/tests/onservertest1.json') == false) {
-    echo 'Тест 1 - не найден или не загружен!<br>';
-} else {
-    echo 'Тест 1 - доступен: <a href="test.php?testnumber=t1">приступить к тесту.</a><br>';
+$list = scandir('./tests');
+
+if (empty($list['2'])) {
+    echo 'Тесты не загружены! Вернитесь на главную старницу для загрузки';
 }
-
-if (@file_get_contents(__DIR__ . '/tests/onservertest2.json') == false) {
-    echo 'Тест 2 - не найден или не загружен!<br>';
-} else {
-    echo 'Тест 2 - доступен: <a href="test.php?testnumber=t2">приступить к тесту.</a><br>';
-}
-
-echo '<br>Для загрузки нового теста вернитесь на <a href="admin.php">главную страницу</a><br>';
+echo 'Список доступных тестов:';
 
 ?>
+
+<html>
+<head>
+    <title>Список тестов</title>
+</head>
+<body>
+    <ul>
+    <?php for ($i=2; $i < count($list); $i++) : ?>   
+        <li><a href="test.php?testnumber=<?php echo $list[$i] ?>">Тест №<?php echo $i-1 ?></a></li>
+    <?php endfor; ?>
+    </ul>
+    
+</body>
+</html>
