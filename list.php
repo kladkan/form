2f69ce848b912b7980af7d23397d74476d208d61
Aleksey <kladkan@yandex.ru>
2018-10-29 22:43:31 +0300

    Update list.php

--- list.php
+++ list.php
@@ -6,6 +6,7 @@ if (empty($list['2'])) {
     echo 'Тесты не загружены! Вернитесь на главную старницу для загрузки';
     exit;
 }
+
 echo 'Список доступных тестов:';
 
 ?>
@@ -17,7 +18,13 @@ echo 'Список доступных тестов:';
 <body>
     <ul>
     <?php for ($i=2; $i < count($list); $i++) : ?>   
-        <li><a href="test.php?testnumber=<?php echo $list[$i] ?>">Тест №<?php echo $i-1 ?></a></li>
+        <li><a href="test.php?testnumber=<?php echo $list[$i] ?>">Тест №<?php echo $i-1 ?></a>
+            <?php
+                $json = file_get_contents(__DIR__ . '/tests/' . $list[$i]);
+                $test = json_decode($json, true);
+                echo ($test['0']['title']);
+            ?>
+        </li>
     <?php endfor; ?>
     </ul>
     
