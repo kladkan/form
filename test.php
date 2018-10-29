69920c5f7a9ad756ebf0a44ec16bfeb6b36e1a8a
Aleksey <kladkan@yandex.ru>
2018-10-29 21:56:05 +0300

    Update test.php

--- test.php
+++ test.php
@@ -8,20 +8,32 @@ if (!empty($_POST)) {
   $forcheck = file_get_contents(__DIR__ . '/tests/' . $testnum);
   $check = json_decode($forcheck, true);
 
+/*
   if (count($_POST) !== count($check)) {
     echo 'Вы ответили не на все вопросы. <a href="list.php">Перейти к списку тестов.</a>';
     exit;
   }
+*/
+/*
+echo '<pre>';
+print_r($_POST);
+print_r($check);
+echo '</pre>';
 
-  $i = 0;
-  foreach ($_POST as $key => $value) {
+print_r(substr(key($_POST), 0, -1));
+*/
+ 
 
-    if ($value == $check[$i]['trueans']) {
-      echo $check[$i]['q'].' Ваш ответ: '. $_POST[$key].' верный.<br>';
+
+  foreach ($check as $key => $value) {
+    if (empty($_POST[substr(key($_POST), 0, -1).$key])) {
+      echo $check[$key]['q'].' Вы не ответили на этот вопрос.<br>';
+    } elseif ($_POST[substr(key($_POST), 0, -1).$key] == $check[$key]['trueans']) {
+      echo $check[$key]['q'].' Ваш ответ: '. $_POST[substr(key($_POST), 0, -1).$key].' верный.<br>';
     } else {
-      echo $check[$i]['q'].' Ваш ответ: '. $_POST[$key].' не верный.<br>';
+      echo $check[$key]['q'].' Ваш ответ: '. $_POST[substr(key($_POST), 0, -1).$key].' не верный.<br>';
     }
-    $i++;
+    
   }
   exit;
 }
@@ -45,7 +57,7 @@ for ($i=2; $i < count($list); $i++) {
     <fieldset>
       <legend><?php echo $val['q'] ?></legend>
       <?php foreach ($val['answer'] as $row => $ans) : ?>
-        <label><input type="radio" name="<?php echo $_GET['testnumber'].'-' ?>q<?php echo $key+1 ?>" value="<?php echo $ans ?>"><?php echo $ans ?></label>
+        <label><input type="radio" name="<?php echo $_GET['testnumber'].'-' ?>q<?php echo $key ?>" value="<?php echo $ans ?>"><?php echo $ans ?></label>
       <?php endforeach; ?>
     </fieldset>
   <?php endforeach; ?>
