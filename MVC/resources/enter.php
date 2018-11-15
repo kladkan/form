95ba8fdd2fa2adc0e429502744bc92b0e733c282
Aleksey <kladkan@yandex.ru>
2018-11-15 19:09:30 +0300

    Update MVC/resources/enter.php

---
+++ MVC/resources/enter.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Авторизация</title>
+</head>
+<body>
+    <form action="index2.php" method="POST">
+        <fieldset>
+        <legend>ФАРМА ДЛЯ ВХОДА</legend>
+            <p>Чтобы войти, введите логин и пароль или пройдите<a href="index2.php?reg=reg"> Регистрацию</a></p>
+            <p>Логин: <input type="text" name="authname"></p>
+            <p>Пароль: <input type="password" name="authpass"></p>
+            <p><input type="submit" value="Войти"> 
+        </fieldset>
+    </form>
+</body>
+</html>
