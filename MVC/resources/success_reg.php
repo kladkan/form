b0130225231e6c46de991d5db9ebc460e983617c
Aleksey <kladkan@yandex.ru>
2018-11-15 19:09:51 +0300

    Update MVC/resources/success_reg.php

---
+++ MVC/resources/success_reg.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Успешная регистрация</title>
+</head>
+<body>
+    <p>Вы успешно зарегистрированы!</p>
+    <p><a href="index2.php">Войти с вашей учетной записью</a></p>
+</body>
+</html>
