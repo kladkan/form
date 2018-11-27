6868cc1b96158e465db08dfcaf412a8f6ce52831
Aleksey <kladkan@yandex.ru>
2018-11-27 15:08:26 +0300

    Create themesListEmpty.php

---
+++ diplom/view/themesListEmpty.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8" />
+</head>
+<body>
+        <p>Список тем пуст! Только администраторы могу добавлять темы.</p>
+</body>
+</html>
