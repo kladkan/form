3bab365affe6a2f342c1f6a45e2c9a6bcc4c17d8
Aleksey <kladkan@yandex.ru>
2018-11-28 16:26:08 +0300

    Create buttonForAsk.php

---
+++ diplom2/View/buttonForAsk.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8" />
+</head>
+<body>
+    <p><a href="index.php?ask_question=ask_question">Задать вопрос</a></p>
+</body>
+</html>
