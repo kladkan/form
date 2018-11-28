16276043d7ed9702665eed268918639059a1f617
Aleksey <kladkan@yandex.ru>
2018-11-28 16:26:12 +0300

    Create emptyAdmin.php

---
+++ diplom2/View/emptyAdmin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8" />
+    <title>Администратор не найден</title>
+</head>
+<body>
+    <p>Администратор не найден или неправильно введён логин/пароль!</p>
+    <p><a href="index.php?admin=admin">Войдите заново</a> или обратитесь к главному Администратору</a></p>
+</body>
+</html>
