ae438397b670ac533695aa14cb7e89a2bf135333
Aleksey <kladkan@yandex.ru>
2018-12-03 14:59:52 +0300

    Create QuestionsTable.php

---
+++ diplom/Model/QuestionsTable.php
@@ -0,0 +1,111 @@
+<?php
+class QuestionsTable
+{
+    //подсчет вопросов в теме
+    function countAllQuesInThemeArray($param)//9
+    {
+        $sql = "SELECT COUNT(*) as 'Вопросов в теме' FROM `questions` WHERE `theme_id`='$param' GROUP BY `theme_id`";
+        $countAllQuesInThemeArray = db()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        return $countAllQuesInThemeArray;
+    }
+
+    //подсчет опубликованных вопросов в теме
+    function countPublishedQuesInThemeArray($param)//10
+    {
+        $sql = "SELECT COUNT(*) AS 'Опубликовано вопросов' FROM `questions` WHERE `theme_id`='$param' AND `published`=1 GROUP BY `theme_id`";
+        $countPublishedQuesInThemeArray = db()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        return $countPublishedQuesInThemeArray;
+    }
+
+    //подсчет вопросов без ответа в теме
+    function countUnansweredQuesInThemeArray($param)//11
+    {
+        $sql = "SELECT COUNT(*) AS 'Вопросов без ответа' FROM `questions` WHERE `theme_id`='$param' AND (`answer` IS NULL OR `answer`='') GROUP BY `theme_id`";
+        $countUnansweredQuesInThemeArray = db()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        return $countUnansweredQuesInThemeArray;
+    }
+
+    //Получение списка вопросов в выбранной теме
+    function questions($param)//12
+    {
+        $sql = "SELECT `questions`.`id`,  `questions`.`theme_id`, `theme`, `question`, `answer`, `date_added`, `published` FROM `questions` JOIN `themes` ON `themes`.`id`=`questions`.`theme_id` WHERE `theme_id`='$param'";
+        $questions = db()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        return $questions;
+    }
+
+    //Получение данных вопроса
+    function showQuestion($param)//13
+    {
+        $sql = "SELECT `questions`.`id`,  `questions`.`theme_id`, `date_added`, `author_name`, `theme`, `question`, `answer`, `published` FROM `questions` JOIN `themes` ON `themes`.`id`=`questions`.`theme_id` WHERE `questions`.`id`='$param'";
+        $showQuestion = db()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        return $showQuestion;
+    }
+
+    //Добавление вопроса
+    function newQuestion($params)//14
+    {
+        $stmt = db()->prepare("INSERT INTO `questions`(`author_name`, `e-mail`, `theme_id`, `question`) VALUES (?, ?, ?, ?)");
+        $stmt->bindParam(1, $params['author_name']);
+        $stmt->bindParam(2, $params['e-mail']);
+        $stmt->bindParam(3, $params['theme_id']);
+        $stmt->bindParam(4, $params['question']);
+        $stmt->execute();
+    }
+
+    //Удаление вопросов одной темы
+    function delQuestionsInTheme($param)//1
+    {
+        $stmt = db()->prepare("DELETE FROM `questions` WHERE `theme_id`='$param'");
+        $stmt->execute();
+    }
+
+    //Переключатель опубликован/скрыт
+    function publishedOnOff($params)//2
+    {
+        $stmt = db()->prepare("UPDATE `questions` SET `published`='{$params['publishedOnOff']}' WHERE `id`='{$params['showQuestionId']}' LIMIT 1");
+        $stmt->execute();
+    }
+
+    //Удаление вопроса из темы
+    function delQuestion($param)//3
+    {
+        $stmt = db()->prepare("DELETE FROM `questions` WHERE `id`='{$param}'");
+        $stmt->execute();
+    }
+
+    //Изменение автора
+    function changeAuthorName($params)//4
+    {
+        $stmt = db()->prepare("UPDATE `questions` SET `author_name`='{$params['changeAuthorName']}' WHERE `id`='{$params['showQuestionId']}' LIMIT 1");
+        $stmt->execute();
+    }
+
+    //Изменение вопроса
+    function changeQuestion($params)//5
+    {
+        $stmt = db()->prepare("UPDATE `questions` SET `question`='{$params['changeQuestion']}' WHERE `id`='{$params['showQuestionId']}' LIMIT 1");
+        $stmt->execute();
+    }
+
+    //Изменение ответа
+    function changeAnswer($params)//6
+    {
+        $stmt = db()->prepare("UPDATE `questions` SET `answer`='{$params['changeAnswer']}' WHERE `id`='{$params['showQuestionId']}' LIMIT 1");
+        $stmt->execute();
+    }
+
+    //Изменение темы
+    function changeTheme($params)//7
+    {
+        $stmt = db()->prepare("UPDATE `questions` SET `theme_id`='{$params['changeThemeId']}' WHERE `id`='{$params['showQuestionId']}' LIMIT 1");
+        $stmt->execute();
+    }
+
+    //Получение всех вопросов без ответа во всех темах в порядке их добавления
+    function unansQuestions()//8
+    {
+        $sql = "SELECT `questions`.`id`, `theme_id`, `theme`, `question`, `answer`, `published`, `author_name`, `e-mail`, `date_added` FROM `questions` JOIN `themes` ON `themes`.`id`=`questions`.`theme_id` WHERE `answer` IS NULL OR `answer`='' ORDER BY `date_added` ASC";
+        $allUnansQuestions = db()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        return $allUnansQuestions;
+    }
+}
