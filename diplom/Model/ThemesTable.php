4f6fc5141870d3dad352d5b942663e3fac9d17fe
Aleksey <kladkan@yandex.ru>
2018-12-05 13:03:07 +0300

    Update ThemesTable.php

--- diplom/Model/ThemesTable.php
+++ diplom/Model/ThemesTable.php
@@ -1,18 +1,24 @@
 <?php
 class ThemesTable
 {
+    private $db;
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
     //Получение списка тем (для всех)
     public function getThemes()
     {
         $sql = "SELECT * FROM `themes`";
-        $themes = db()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        $themes = $this->db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
         return $themes;
     }
 
     //Добавление новой темы
     public function addTheme($param)
     {
-        $stmt = db()->prepare("INSERT INTO `themes` (`theme`) VALUES (?)");
+        $stmt = $this->db->prepare("INSERT INTO `themes` (`theme`) VALUES (?)");
         $stmt->bindParam(1, $param);
         $stmt->execute();
     }
@@ -20,7 +26,7 @@ class ThemesTable
     //Удаление темы со всеми вопросами
     public function delTheme($param)//22222222222222
     {
-        $stmt = db()->prepare("DELETE FROM `themes` WHERE `id`='$param'");
+        $stmt = $this->db->prepare("DELETE FROM `themes` WHERE `id`='$param'");
         $stmt->execute();
     }
 }
